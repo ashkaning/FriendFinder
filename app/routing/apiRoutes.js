9c4d410f622c606960710a78364258f7520f07c5
ashkan <ashkan.ram70@gmail.com>
2019-09-13 20:29:12 -0700

    fixed a bug

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,7 +3,9 @@ var people = require("../data/friends");
 
 module.exports = function(app) {
 
-  
+    app.get("/api/friends", function(req, res) {
+        res.json(people);
+    });
     app.post("/api/friends", function(req, res) {
         res.json(people);
     });
